2088e5ce1bb8169b2a9f902bbe4165bc1186b880
scottseo000 <scottseo96@gmail.com>
2024-09-26 11:57:01 -0400

    Implemented performance profiler that measures time to execute methods. To pass test mvn -Dtest=ProfilerImplTest. Should also pass mvn test

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
@@ -7,7 +7,9 @@ import java.nio.file.Path;
 import java.time.Clock;
 import java.time.ZonedDateTime;
 import java.util.Objects;
-
+import java.lang.reflect.*;
+import java.lang.annotation.Annotation;
+import java.util.*;
 import static java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME;
 
 /**
@@ -28,12 +30,32 @@ final class ProfilerImpl implements Profiler {
   @Override
   public <T> T wrap(Class<T> klass, T delegate) {
     Objects.requireNonNull(klass);
-
     // TODO: Use a dynamic proxy (java.lang.reflect.Proxy) to "wrap" the delegate in a
     //       ProfilingMethodInterceptor and return a dynamic proxy from this method.
     //       See https://docs.oracle.com/javase/10/docs/api/java/lang/reflect/Proxy.html.
 
-    return delegate;
+    //Wrap should throw exception if wrapped interface does not have a @Profiled method
+    Method[] methods = klass.getDeclaredMethods();
+    boolean containsProfiledAnnotation = false;
+    //Check each method's annotations
+    for (Method m : methods) {
+      Annotation[] annotationList = m.getAnnotations();
+      //Check all annotations retrieved
+      for (Annotation a : annotationList) {
+        if (a instanceof Profiled) {
+          containsProfiledAnnotation = true;
+        }
+      }
+    }
+
+    //If Profiled annotation was not found, it must throw an IllegalArgumentException
+    if (!containsProfiledAnnotation) {throw new IllegalArgumentException();}
+    else {
+      return (T) Proxy.newProxyInstance(
+              klass.getClassLoader(),
+              new Class[]{klass},
+              new ProfilingMethodInterceptor(clock, delegate, state));
+    }
   }
 
   @Override
--- starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
@@ -1,8 +1,9 @@
 package com.udacity.webcrawler.profiler;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.time.Clock;
+import java.time.*;
 import java.util.Objects;
 
 /**
@@ -12,19 +13,55 @@ import java.util.Objects;
 final class ProfilingMethodInterceptor implements InvocationHandler {
 
   private final Clock clock;
+  private final Object target;
+  private final ProfilingState state;
 
   // TODO: You will need to add more instance fields and constructor arguments to this class.
-  ProfilingMethodInterceptor(Clock clock) {
+  ProfilingMethodInterceptor(Clock clock, Object target, ProfilingState state) {
     this.clock = Objects.requireNonNull(clock);
+    this.target = target;
+    this.state = state;
   }
 
   @Override
-  public Object invoke(Object proxy, Method method, Object[] args) {
+  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable{
     // TODO: This method interceptor should inspect the called method to see if it is a profiled
     //       method. For profiled methods, the interceptor should record the start time, then
     //       invoke the method using the object that is being profiled. Finally, for profiled
     //       methods, the interceptor should record how long the method call took, using the
     //       ProfilingState methods.
-    return null;
+    Instant start = null;
+    Object methodTest;
+    //Method must be annotated with @Profiled
+    if (method.getAnnotation(Profiled.class) == null) {
+      start = clock.instant();
+      //This should throw an exception
+      try {
+        methodTest = method.invoke(target, args);
+      } catch(InvocationTargetException e) {
+        throw e.getTargetException();
+      } catch(Throwable t) {
+        throw new RuntimeException(t);
+      } finally {
+        //Record time anyways regardless of exception
+        Duration timeToExecute = Duration.between(start, clock.instant());
+        state.record(target.getClass(), method, timeToExecute);
+      }
+    } else {
+      start = clock.instant();
+      //This should not throw an exception
+      try {
+        methodTest = method.invoke(target, args);
+      } catch(InvocationTargetException e) {
+        throw e.getTargetException();
+      } catch(Throwable t){
+        throw new RuntimeException(t);
+      } finally {
+        //Record time anyways regardless of exception
+        Duration timeToExecute = Duration.between(start, clock.instant());
+        state.record(target.getClass(), method, timeToExecute);
+      }
+    }
+    return methodTest;
   }
 }
